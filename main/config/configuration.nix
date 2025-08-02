be1223de34e199b750aa63b14253ce37cecb9552
sid <98341483+mooofin@users.noreply.github.com>
2025-08-02 23:54:55 +0530

    lana nixies

--- main/config/configuration.nix
+++ main/config/configuration.nix
@@ -1,6 +1,6 @@
-
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+```nix
+# This file defines what should be installed on your system.
+# Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
@@ -10,20 +10,20 @@
     ./hardware-configuration.nix
   ];
 
-#  system.stateVersion = "25.05";
+  # Use the latest stable release of NixOS.
+  system.stateVersion = "24.05";
 
-  # Bootloader
+  # --- Bootloader ---
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
-  # Hostname and networking
+
+  # --- Hostname and Networking ---
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Time zone
+  # --- Time zone and Locale ---
   time.timeZone = "Asia/Kolkata";
 
-  # Locale
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocales = [ "en_US.UTF-8/UTF-8" "en_IN/UTF-8" ];
@@ -40,56 +40,44 @@
     };
   };
 
-  # Keyboard layout
+  # --- Keyboard Layout ---
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable X11 and SDDM display manager
-#  services.xserver.enable = true;
-#  services.displayManager.sddm.enable = true;
+  # --- Flake and nix-ld configuration ---
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  programs.nix-ld.enable = true;
 
-  # Auto-login
+  # --- Graphics and Display Manager ---
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "muffin";
 
-  # Enable Hyprland window manager
+  # --- Window Manager ---
   programs.hyprland.enable = true;
 
-  # Printing
-  services.printing.enable = true;
-
-  #flakes-expemental
-  
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  #nix-ld
-  programs.nix-ld.enable = true;
- 
-
- 
-
-
-
-
-  # Audio with PipeWire (replacing PulseAudio)
- # hardware.pulseaudio.enable = false;
- # security.rtkit.enable = true;
- # services.pipewire = {
- #   enable = true;
- #   alsa.enable = true;
- #   alsa.support32Bit = true;
- #   pulse.enable = true;
+  # --- Audio ---
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
     # jack.enable = true;
-#  };
+  };
 
-  # Bluetooth support
+  # --- Bluetooth ---
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
-  # User account
+  # --- Printing ---
+  services.printing.enable = true;
+
+  # --- User Accounts ---
   users.users.muffin = {
     isNormalUser = true;
     description = "muffin";
@@ -101,124 +89,93 @@
     ];
   };
 
-  # Allow unfree packages
+  # --- Package Management ---
   nixpkgs.config.allowUnfree = true;
 
-  # Enable Firefox
+  # --- Program Settings ---
   programs.firefox.enable = true;
+  programs.zsh.enable = true;
+  environment.etc."zshenv".text = ''
+    export ZDOTDIR="$HOME/.config/zsh"
+  '';
 
-  # System packages
+  # --- System Packages ---
   environment.systemPackages = with pkgs; [
+    # Terminal and Shell
+    zsh
+    fish
     kitty
-    waybar
-    wofi
-    swww
-    brightnessctl
-    pamixer
-    playerctl
-    wl-clipboard
-    pavucontrol
-    networkmanager
     fastfetch
     cmatrix
     btop
-    vscode
-    discord
-    spotify
-    grim
-    slurp
-    capitaine-cursors
-    xfce.thunar
-    xfce.tumbler
-    pipes
+    unzip
+    git
+    git
+    zoxide
+    fzf
+    # Hyprland and Wayland Tools
+    waybar
+    wofi
+    rofi
+    rofi-wayland
+    swww
+    swaylock
     hyprlock
     hypridle
-    cliphist
+    hyprpaper
+    waypaper
+    grim
+    slurp
+    swappy
+    wl-clipboard
+    # File Management
+    xfce.thunar
+    xfce.tumbler
+    # Audio and Bluetooth
+    pavucontrol
+    pamixer
+    playerctl
+    cava
     bluetuith
     bluez
     bluez-tools
     blueman
-    pipes
-    cava
-    wget	
-    rofi
-    unzip
-    zoxide    
-    rofi-wayland
-    pkgs.obsidian
-    python3Full    
+    # System Utilities
+    brightnessctl
+    ddcutil
+    lm_sensors
+    wget
+    libqalculate
+    # Applications
+    vscode
+    discord
+    spotify
+    obsidian
+    binaryninja-free
+    ghidra
+    burpsuite
+    # Programming
+    rustc
+    cargo
+    gcc
+    gnumake
+    python3Full
     python3Packages.tqdm
-    pkgs.binaryninja-free
-    hyprpaper
-    go 
-    git
-    git gcc gnumake
-  ddcutil
-  brightnessctl
-  lm_sensors
-  fish
-  aubio
-  qt6.qtdeclarative
-  material-symbols
-  swappy
-  libqalculate
-#  app2unit
-  burpsuite
-  rustc
-  cargo
-  fzf
-   pkgs.hyprpaper
+    # Visuals and Themes
+    capitaine-cursors
+    material-symbols
+    # Other
+    pipes
+    aubio
+    qt6.qtdeclarative
+  ];
 
- ];
-
-  services.displayManager.sddm.enable = true;
-  services.xserver.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
+  # --- Final Configuration ---
   # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-  
-
-   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  
-
-programs.zsh.enable = true;
-
-environment.etc."zshenv".text = ''
-  export ZDOTDIR="$HOME/.config/zsh"
-'';
-
-
-
-
-
-
-
-
-
-  } 
+  # settings for stateful data were taken.
+  # It's recommended to leave this value at the release version of the
+  # first install of this system.
+  # Before changing this value, read the documentation for this option.
+  system.stateVersion = "25.05";
+}
+```
