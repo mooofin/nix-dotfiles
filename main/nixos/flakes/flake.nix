23e49f7482547b56e8b336eef216a06979212db2
sid <98341483+mooofin@users.noreply.github.com>
2025-09-16 18:55:10 +0530

    Update flake.nix

--- main/nixos/flakes/flake.nix
+++ main/nixos/flakes/flake.nix
@@ -1,26 +1,26 @@
-
 {
-  description = "An example NixOS configuration"; 
+  description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
-    nur = { url = "github:nix-community/NUR"; };
-  };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = inputs:
-    /* ignore:: */ let ignoreme = ({config,lib,...}: with lib; { system.nixos.revision = mkForce null; system.nixos.versionSuffix = mkForce "pre-git"; }); in
-  {
-    nixosConfigurations = {
-
-      mysystem = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-
-          /* ignore */ ignoreme # ignore this; don't include it; it is a small helper for this example
-        ];
-        specialArgs = { inherit inputs; };
-      };
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nixpkgs, caelestia-shell, ... }@inputs:
+    let
+      system = "x86_64-linux";               
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+
+        specialArgs = { inherit inputs system; };
+      };
+    };
 }
